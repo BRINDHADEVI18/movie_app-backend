bf5e920d0cc42b00c8703e3de9f91f5933697edf
BRINDHADEVI18 <64881981+BRINDHADEVI18@users.noreply.github.com>
2026-09-21 22:26:15 +0530

    Create db.js

---
+++ src/db.js
@@ -0,0 +1,17 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+pool.connect()
+  .then(() => console.log("PostgreSQL connected"))
+  .catch((err) => console.error("PostgreSQL connection error:", err));
+
+export default pool;
